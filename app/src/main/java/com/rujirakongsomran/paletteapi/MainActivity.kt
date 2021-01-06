405ee4d6275e00a418230eb2a646a424cb8054a3
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-01-06 23:05:30 +0700

    Display color each swatch

--- app/src/main/java/com/rujirakongsomran/paletteapi/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/paletteapi/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.rujirakongsomran.paletteapi
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.palette.graphics.Palette
 import com.rujirakongsomran.paletteapi.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,5 +18,43 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
 
         setContentView(binding.root)
+
+        //val bitmap: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.blueberries)
+
+        createPaletteAsync((ContextCompat.getDrawable(this, R.drawable.blueberries) as BitmapDrawable).bitmap)
+
     }
+
+    fun createPaletteAsync(bitmap: Bitmap) {
+        Palette.from(bitmap).generate { palette ->
+            // Use generated instance
+
+            binding.tvDarkMuted.apply {
+                setTextColor(palette?.darkMutedSwatch!!.titleTextColor)
+                setBackgroundColor(palette.darkMutedSwatch!!.rgb)
+            }
+            binding.tvDarkVibrant.apply {
+                setTextColor(palette?.darkVibrantSwatch!!.titleTextColor)
+                setBackgroundColor(palette.darkVibrantSwatch!!.rgb)
+            }
+            binding.tvLightMuted.apply {
+                setTextColor(palette?.lightMutedSwatch!!.titleTextColor)
+                setBackgroundColor(palette.lightMutedSwatch!!.rgb)
+            }
+            binding.tvLightVibrant.apply {
+                setTextColor(palette?.lightVibrantSwatch!!.titleTextColor)
+                setBackgroundColor(palette.lightVibrantSwatch!!.rgb)
+            }
+            binding.tvMuted.apply {
+                setTextColor(palette?.mutedSwatch!!.titleTextColor)
+                setBackgroundColor(palette.mutedSwatch!!.rgb)
+            }
+            binding.tvVibrant.apply {
+                setTextColor(palette?.vibrantSwatch!!.titleTextColor)
+                setBackgroundColor(palette.vibrantSwatch!!.rgb)
+            }
+
+        }
+    }
+
 }
